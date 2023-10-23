4bedae6238dab4c5adda0e2c90b966bd81e1f440
Hajin Jang <jb6804@naver.com>
2023-10-23 12:53:04 +0900

    Prepare v2.1 release (again)

--- resource.h
+++ resource.h
@@ -6,16 +6,16 @@
 #define IDC_STATIC                  -1
 
 // resource.rc
-#define RES_FILEVERSION             2,0,0,0
-#define RES_FILEVERSION_STR         "2.0.0.0\0"
+#define RES_FILEVERSION             2,1,0,0
+#define RES_FILEVERSION_STR         "2.1.0.0\0"
 
-#define RES_PRODUCTVERSION          2,0,0,0
-#define RES_PRODUCTVERSION_STR      "2.0\0"
+#define RES_PRODUCTVERSION          2,1,0,0
+#define RES_PRODUCTVERSION_STR      "2.1\0"
 
 #define RES_COMPANYNAME_STR         "Joveler\0"
 #define RES_FILEDESCRIPTION_STR     "BatteryLine\0"
 #define RES_INTERNALNAME_STR        "BatteryLine\0"
-#define RES_LEGALCOPYRIGHT_STR      "Copyright (c) 2016-2017 Joveler\0"
+#define RES_LEGALCOPYRIGHT_STR      "Copyright (c) 2016-2021 Joveler\0"
 #define RES_LEGALTRADEMARKS1_STR    "All Rights Reserved\0"
 #define RES_LEGALTRADEMARKS2_STR    RES_LEGALTRADEMARKS1_STR
 #define RES_ORIGINALFILENAME_STR    "BatteryLine.exe\0"
